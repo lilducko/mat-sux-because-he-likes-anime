8ec8d85f1f0d65a7db984633f5a1e659bc0d4a22
lilducko <tobinthadathil@gmail.com>
2019-12-03 20:02:10 -0500

    it is the roll a ball thing
    the ball rolls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroler : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidbody.AddForce(movement); 
+    }
+}
